e06a42586aa1684a4e8a1f1731fdbf4560f1f421
Pratik Raj Verma <erpratikrajverma@gmail.com>
2025-07-01 18:05:43 +0530

    Class 6

---
+++ Class 6/JS Basics/script.js
@@ -0,0 +1,114 @@
+// let x = 20;
+// // let x = 30;  //redeclare
+// x = 30; //reassign 
+
+// console.log(x)
+
+
+// var x = 30;
+
+// // var x = 40;
+
+// x = 60
+// console.log(x)
+
+// const x = 30
+// // x = 40;
+// const x = 60;
+
+// console.log(x)
+
+
+// let x = 30;
+
+// let x = 'pratik'
+
+// let x = true
+
+// let x;
+
+// let x = null
+
+// console.log(typeof(x))
+
+
+// let x = 10;
+// let y = 20;
+// console.log(x *  y)
+
+
+// let x = '10';
+
+// let y = 10;
+
+// console.log(x != y)
+
+// let x = 10;
+// let y = 20
+
+// // console.log(x>y)
+// console.log(x<y)
+
+
+
+// {
+//     let x  = 10
+//     console.log(x)
+// }
+
+// let x  = 20
+// console.log(x)
+
+
+
+// var x = 20
+
+// var x = 30
+
+
+// console.log(x)
+
+
+ 
+// var x = 30
+// let z  = 40
+// var y = 30
+
+// let x = 500;
+// let y = 30;
+
+// if(x<y)
+// {
+//     console.log('this condition satisfy')
+// }else{
+//     console.log('not satisfy')
+// }
+
+
+
+
+// for(let i = 1 ; i<=10; i++){
+//     // console.log('hello world')
+//     if(i==5){
+//         // break
+//         continue
+//     }
+//     console.log(i)
+// }
+
+
+// let x = 1;
+// let y = 10
+
+// while(x<y){
+//     console.log('hello world...');
+//     x++;
+// }
+
+
+
+let firstname = 'pratik'
+
+// console.log(firstname + ' Raj Verma')
+
+console.log(`${firstname} Raj Verma`)
---
+++ Class 6/function/script.js
@@ -0,0 +1,124 @@
+// function add(a,b){      //function define
+//     return a+b
+// }
+
+// let result  =  add(10,20) //function calling
+
+// console.log(result)
+
+// console.log(add(20,30))
+
+
+// function substract(a,b){
+//     return a-b
+//     // console.log(a-b)
+    
+// }
+
+// let result = substract(10,5)
+// console.log(result)
+
+
+
+// let x = 20;
+
+// let div = function(){
+//     console.log('hello world')
+// }
+
+// div()
+
+
+// function parent(){              //higher order function......
+//     return function(){          //first class function
+//         console.log('child function')
+//     }
+//     // return 10
+// }
+
+// let div = parent()
+// div()
+// console.log(div)
+
+// function child(){           //first class function
+//     console.log('this is child ')
+// }
+
+// function parent(fun){ //higher order function
+//     fun()
+// }
+
+// parent(child)
+
+
+
+// let div = function(){                //regular function....
+//     console.log('this is div function')
+// }
+
+// div()
+
+// let div = ()=>{          //arrow functionj..........
+//     console.log('this is div function')
+// }
+
+// div()
+
+
+
+// div()        //Hoisting...............
+
+
+// function div(){
+//     console.log('this is div function....')
+// }
+
+ 
+
+// console.log(x)
+// var x = 20
+
+
+// console.log(y)
+// var y = 20
+
+
+// div()
+
+// let div = function(){
+//     console.log('this is div')
+// }
+// var div = function(){
+//     console.log('this is div')
+// }
+
+// function div(){
+//     console.log('this is div')
+// }
+
+// let div = () => {
+//     console.log('this is div')
+// }
+
+
+// console.log(x)
+
+// let  x = 10
+
+// function child()
+// {
+//     console.log('this is child')
+// }
+
+// function parent(callback){
+//     console.log('this is parent')
+//     callback()
+// }
+
+// parent(()=>{
+//     console.log('this is child')
+// })
+
+// parent(function(){
+//     console.log('this is child')
+// })
